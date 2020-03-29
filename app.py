c77577b136951a254db5bca61a93ab3586ed1429
German Lugo <german.lugo@bitnibs.com>
2020-03-29 12:17:32 +0200

    Initial commit.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from pydub import AudioSegment
 
-sound = AudioSegment.from_mp3("test.mp3")
+sound = AudioSegment.from_mp3("Sample.mp3")
 
 # sound._data is a bytestring
 raw_data = sound._data
+
+print(raw_datagit)
